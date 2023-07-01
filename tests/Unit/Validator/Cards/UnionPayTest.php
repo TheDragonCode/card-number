dd919c4b83de4a14742b2d2224d5caffde2c041e
Andrey Helldar <helldar@dragon-code.pro>
2023-07-01 17:19:00 +0300

    Added tests for cards

---
+++ tests/Unit/Validator/Cards/UnionPayTest.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+use DragonCode\CardNumber\Enums\CardType;
+
+it('must be an valid card number', function () {
+    isValid('6218 6843 8138 8467', CardType::unionPay);
+    isValid('6 2166 8463 1384 6863', CardType::unionPay);
+    isValid('62 6848 4316 8716 1443', CardType::unionPay);
+    isValid('626 5146 8438 3138 4681', CardType::unionPay);
+    isValid('629 1684 3483 1568 4683', CardType::unionPay);
+    isValid('6218 6843 8138 8467', CardType::unionPay);
+});
+
+it('must be an invalid card number', function () {
+    isInvalid('0247 3140 4123 4566', CardType::unionPay);
+    isInvalid('5019 1404 0123 4564', CardType::unionPay);
+
+    isInvalid('4019 1404 0123 4567', CardType::unionPay);
+    isInvalid('30 4687 9012 3452', CardType::unionPay);
+    isInvalid('30 6687 9012 3457', CardType::unionPay);
+    isInvalid('6011 4951 2345 6788', CardType::unionPay);
+    isInvalid('6221 2649 5123 6787', CardType::unionPay);
+    isInvalid('6229 2549 5123 6780', CardType::unionPay);
+    isInvalid('5011 4951 2345 6780', CardType::unionPay);
+    isInvalid('6004 1404 0000 1233', CardType::unionPay);
+    isInvalid('5004 1404 0000 1235', CardType::unionPay);
+    isInvalid('6 0628 2123 4568', CardType::unionPay);
+    isInvalid('6062 8212 3456 7890', CardType::unionPay);
+    isInvalid('384 1123 4567 8912 3452', CardType::unionPay);
+    isInvalid('5 0628 2123 4569', CardType::unionPay);
+    isInvalid('5062 8212 3456 7892', CardType::unionPay);
+    isInvalid('284 1123 4567 8912 3453', CardType::unionPay);
+    isInvalid('2131 1356 1381 3510', CardType::unionPay);
+    isInvalid('1 8001 3513 8438 4382', CardType::unionPay);
+    isInvalid('35 1868 4438 1138 4890', CardType::unionPay);
+    isInvalid('213 1681 6843 5681 3897', CardType::unionPay);
+    isInvalid('1131 1356 1381 3512', CardType::unionPay);
+    isInvalid('1 7001 3513 8438 4384', CardType::unionPay);
+    isInvalid('25 1868 4438 1138 4892', CardType::unionPay);
+    isInvalid('212 1681 6843 5681 3898', CardType::unionPay);
+    isInvalid('5018 6138 3377', CardType::unionPay);
+    isInvalid('5 0568 4131 3585', CardType::unionPay);
+    isInvalid('58 3813 5138 6813', CardType::unionPay);
+    isInvalid('561 6816 8138 4135', CardType::unionPay);
+    isInvalid('5036 8131 3841 6485', CardType::unionPay);
+    isInvalid('5 6396 8434 3168 4650', CardType::unionPay);
+    isInvalid('56 1168 6431 6816 8439', CardType::unionPay);
+    isInvalid('639 1684 3483 1568 4681', CardType::unionPay);
+    isInvalid('4018 6138 3370', CardType::unionPay);
+    isInvalid('5 0168 4131 3589', CardType::unionPay);
+    isInvalid('54 3813 5138 6817', CardType::unionPay);
+    isInvalid('551 6816 8138 4137', CardType::unionPay);
+    isInvalid('5016 8131 3841 6489', CardType::unionPay);
+    isInvalid('5 5396 8434 3168 4652', CardType::unionPay);
+    isInvalid('55 1168 6431 6816 8430', CardType::unionPay);
+    isInvalid('5413 5168 4131 5315', CardType::unionPay);
+    isInvalid('2351 6843 5686 8432', CardType::unionPay);
+    isInvalid('2229 6516 1681 3680', CardType::unionPay);
+    isInvalid('2720 1684 6416 1843', CardType::unionPay);
+    isInvalid('5613 5168 4131 5313', CardType::unionPay);
+    isInvalid('2951 6843 5686 8436', CardType::unionPay);
+    isInvalid('2929 6516 1681 3683', CardType::unionPay);
+    isInvalid('2730 1684 6416 1841', CardType::unionPay);
+    isInvalid('2201 6868 4646 8444', CardType::unionPay);
+    isInvalid('2301 6868 4646 8443', CardType::unionPay);
+    isInvalid('9646 8166 1676 5137', CardType::unionPay);
+    isInvalid('8646 8166 1676 5139', CardType::unionPay);
+    isInvalid('9792 0013 8138 4354', CardType::unionPay);
+    isInvalid('9792 8913 8138 4358', CardType::unionPay);
+    isInvalid('6318 6843 8138 8466', CardType::unionPay);
+    isInvalid('6221 2668 4648 6162', CardType::unionPay);
+    isInvalid('6121 2568 4648 6164', CardType::unionPay);
+    isInvalid('6 1166 8463 1384 6865', CardType::unionPay);
+    isInvalid('61 6848 4316 8716 1444', CardType::unionPay);
+    isInvalid('616 5146 8438 3138 4683', CardType::unionPay);
+    isInvalid('4026 8434 8316 8683', CardType::unionPay);
+    isInvalid('4 1750 0123 1838 4345', CardType::unionPay);
+    isInvalid('4 4051 3834 3186 8161', CardType::unionPay);
+    isInvalid('4 5086 1684 3843 1899', CardType::unionPay);
+    isInvalid('4 8444 6483 1381 8683', CardType::unionPay);
+    isInvalid('4 9131 6843 4351 6811', CardType::unionPay);
+    isInvalid('4025 8434 8316 8684', CardType::unionPay);
+    isInvalid('4 1740 0123 1838 4348', CardType::unionPay);
+    isInvalid('4 4031 3834 3186 8166', CardType::unionPay);
+    isInvalid('4 5076 1684 3843 1891', CardType::unionPay);
+    isInvalid('4 8454 6483 1381 8680', CardType::unionPay);
+    isInvalid('4 9181 6843 4351 6810', CardType::unionPay);
+    isInvalid('4 5131 3813 4612', CardType::unionPay);
+    isInvalid('5 5131 3813 4611', CardType::unionPay);
+    isInvalid('5026 8434 8316 8680', CardType::unionPay);
+});
---
+++ tests/Unit/Validator/Cards/VisaElectronTest.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+use DragonCode\CardNumber\Enums\CardType;
+
+it('must be an valid card number', function () {
+    isValid('4026 8434 8316 8683', CardType::visaElectron);
+    isValid('4 1750 0123 1838 4345', CardType::visaElectron);
+    isValid('4 4051 3834 3186 8161', CardType::visaElectron);
+    isValid('4 5086 1684 3843 1899', CardType::visaElectron);
+    isValid('4 8444 6483 1381 8683', CardType::visaElectron);
+    isValid('4 9131 6843 4351 6811', CardType::visaElectron);
+    isValid('4026 8434 8316 8683', CardType::visaElectron);
+});
+
+it('must be an invalid card number', function () {
+    isInvalid('0247 3140 4123 4566', CardType::visaElectron);
+    isInvalid('5019 1404 0123 4564', CardType::visaElectron);
+
+    isInvalid('4019 1404 0123 4567', CardType::visaElectron);
+    isInvalid('30 4687 9012 3452', CardType::visaElectron);
+    isInvalid('30 6687 9012 3457', CardType::visaElectron);
+    isInvalid('6011 4951 2345 6788', CardType::visaElectron);
+    isInvalid('6221 2649 5123 6787', CardType::visaElectron);
+    isInvalid('6229 2549 5123 6780', CardType::visaElectron);
+    isInvalid('5011 4951 2345 6780', CardType::visaElectron);
+    isInvalid('6004 1404 0000 1233', CardType::visaElectron);
+    isInvalid('5004 1404 0000 1235', CardType::visaElectron);
+    isInvalid('6 0628 2123 4568', CardType::visaElectron);
+    isInvalid('6062 8212 3456 7890', CardType::visaElectron);
+    isInvalid('384 1123 4567 8912 3452', CardType::visaElectron);
+    isInvalid('5 0628 2123 4569', CardType::visaElectron);
+    isInvalid('5062 8212 3456 7892', CardType::visaElectron);
+    isInvalid('284 1123 4567 8912 3453', CardType::visaElectron);
+    isInvalid('2131 1356 1381 3510', CardType::visaElectron);
+    isInvalid('1 8001 3513 8438 4382', CardType::visaElectron);
+    isInvalid('35 1868 4438 1138 4890', CardType::visaElectron);
+    isInvalid('213 1681 6843 5681 3897', CardType::visaElectron);
+    isInvalid('1131 1356 1381 3512', CardType::visaElectron);
+    isInvalid('1 7001 3513 8438 4384', CardType::visaElectron);
+    isInvalid('25 1868 4438 1138 4892', CardType::visaElectron);
+    isInvalid('212 1681 6843 5681 3898', CardType::visaElectron);
+    isInvalid('5018 6138 3377', CardType::visaElectron);
+    isInvalid('5 0568 4131 3585', CardType::visaElectron);
+    isInvalid('58 3813 5138 6813', CardType::visaElectron);
+    isInvalid('561 6816 8138 4135', CardType::visaElectron);
+    isInvalid('5036 8131 3841 6485', CardType::visaElectron);
+    isInvalid('5 6396 8434 3168 4650', CardType::visaElectron);
+    isInvalid('56 1168 6431 6816 8439', CardType::visaElectron);
+    isInvalid('639 1684 3483 1568 4681', CardType::visaElectron);
+    isInvalid('4018 6138 3370', CardType::visaElectron);
+    isInvalid('5 0168 4131 3589', CardType::visaElectron);
+    isInvalid('54 3813 5138 6817', CardType::visaElectron);
+    isInvalid('551 6816 8138 4137', CardType::visaElectron);
+    isInvalid('5016 8131 3841 6489', CardType::visaElectron);
+    isInvalid('5 5396 8434 3168 4652', CardType::visaElectron);
+    isInvalid('55 1168 6431 6816 8430', CardType::visaElectron);
+    isInvalid('629 1684 3483 1568 4683', CardType::visaElectron);
+    isInvalid('5413 5168 4131 5315', CardType::visaElectron);
+    isInvalid('2351 6843 5686 8432', CardType::visaElectron);
+    isInvalid('2229 6516 1681 3680', CardType::visaElectron);
+    isInvalid('2720 1684 6416 1843', CardType::visaElectron);
+    isInvalid('5613 5168 4131 5313', CardType::visaElectron);
+    isInvalid('2951 6843 5686 8436', CardType::visaElectron);
+    isInvalid('2929 6516 1681 3683', CardType::visaElectron);
+    isInvalid('2730 1684 6416 1841', CardType::visaElectron);
+    isInvalid('2201 6868 4646 8444', CardType::visaElectron);
+    isInvalid('2301 6868 4646 8443', CardType::visaElectron);
+    isInvalid('9646 8166 1676 5137', CardType::visaElectron);
+    isInvalid('8646 8166 1676 5139', CardType::visaElectron);
+    isInvalid('9792 0013 8138 4354', CardType::visaElectron);
+    isInvalid('9792 8913 8138 4358', CardType::visaElectron);
+    isInvalid('6218 6843 8138 8467', CardType::visaElectron);
+    isInvalid('6 2166 8463 1384 6863', CardType::visaElectron);
+    isInvalid('62 6848 4316 8716 1443', CardType::visaElectron);
+    isInvalid('626 5146 8438 3138 4681', CardType::visaElectron);
+    isInvalid('6318 6843 8138 8466', CardType::visaElectron);
+    isInvalid('6221 2668 4648 6162', CardType::visaElectron);
+    isInvalid('6121 2568 4648 6164', CardType::visaElectron);
+    isInvalid('6 1166 8463 1384 6865', CardType::visaElectron);
+    isInvalid('61 6848 4316 8716 1444', CardType::visaElectron);
+    isInvalid('616 5146 8438 3138 4683', CardType::visaElectron);
+    isInvalid('4025 8434 8316 8684', CardType::visaElectron);
+    isInvalid('4 1740 0123 1838 4348', CardType::visaElectron);
+    isInvalid('4 4031 3834 3186 8166', CardType::visaElectron);
+    isInvalid('4 5076 1684 3843 1891', CardType::visaElectron);
+    isInvalid('4 8454 6483 1381 8680', CardType::visaElectron);
+    isInvalid('4 9181 6843 4351 6810', CardType::visaElectron);
+    isInvalid('4 5131 3813 4612', CardType::visaElectron);
+    isInvalid('5 5131 3813 4611', CardType::visaElectron);
+    isInvalid('5026 8434 8316 8680', CardType::visaElectron);
+});
