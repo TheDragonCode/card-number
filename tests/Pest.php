2d7005489c8fa601b32bba14a5b88d10783c3d9b
Andrey Helldar <helldar@dragon-code.pro>
2023-07-04 21:40:13 +0300

    Removed doc-block

--- tests/Pest.php
+++ tests/Pest.php
@@ -11,17 +11,6 @@
 |
 */
 
-/*
-|--------------------------------------------------------------------------
-| Expectations
-|--------------------------------------------------------------------------
-|
-| When you're writing tests, you often need to check that values meet certain conditions. The
-| "expect()" function gives you access to a set of "expectations" methods that you can use
-| to assert different things. Of course, you may extend the Expectation API at any time.
-|
-*/
-
 use DragonCode\CardNumber\CardNumber;
 use DragonCode\CardNumber\Enums\CardType;
 use DragonCode\CardNumber\Factories\Factory;
