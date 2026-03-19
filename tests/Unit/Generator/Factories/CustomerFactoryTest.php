704681bee00470959784e5ae65157bb4764384ae
Andrey Helldar <helldar@dragon-code.pro>
2026-03-20 00:53:38 +0300

    Update expected values in CustomerFactoryTest

--- tests/Unit/Generator/Factories/CustomerFactoryTest.php
+++ tests/Unit/Generator/Factories/CustomerFactoryTest.php
@@ -12,7 +12,7 @@ it('must be the customer\'s factory', function () {
     $factory2 = CustomerFactory::create()->level(3)->customer(12345);
     $factory3 = CustomerFactory::create()->level(1)->customer(123456);
 
-    generatedEquals($factory1, '250-4001-2343', $formatter);
-    generatedEquals($factory2, '250-3012-3456', $formatter);
-    generatedEquals($factory3, '250-1123-4561', $formatter);
+    generatedEquals($factory1, '260-4001-2343', $formatter);
+    generatedEquals($factory2, '260-3012-3456', $formatter);
+    generatedEquals($factory3, '260-1123-4561', $formatter);
 });
